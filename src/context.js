b9d804cfa938603695cb1094bd5c95a456ead800
olabee100 <olabayoantonio@gmail.com>
2023-05-30 11:21:19 +0100

    context corrected

--- src/context.js
+++ src/context.js
@@ -1,60 +1,52 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Context = React.createContext();
 
 const reducer = (state, action) => {
-    switch(action.type) {
-        case 'DELETE_CONTACT':
-            return {
-                ...state,
-                contacts: state.contacts.filter(contact =>
-                contact.id !== action.payload)
-            };
-        case 'ADD_CONTACT':
-            return {
-                ...state,
-                contacts: [action.payload, ...state.contacts]
-            };
-        case 'UPDATE_CONTACT':
-            return {
-                ...state,
-                contacts: state.contacts.map(contact => contact.id 
-                    === action.payload.id ? (contact 
-                    = action.payload) : contact)
-            };
+  switch (action.type) {
+    case 'DELETE_CONTACT':
+      return {
+        ...state,
+        contacts: state.contacts.filter(contact => contact.id !== action.payload)
+      };
+    case 'ADD_CONTACT':
+      return {
+        ...state,
+        contacts: [action.payload, ...state.contacts]
+      };
+    case 'UPDATE_CONTACT':
+      return {
+        ...state,
+        contacts: state.contacts.map(contact =>
+          contact.id === action.payload.id ? (contact = action.payload) : contact
+        )
+      };
+    default:
+      return state;
+  }
+};
 
-            default:
-                return state;
+export const Provider = ({ children }) => {
+  const [state, setState] = useState({
+    contacts: [],
+    dispatch: action => setState(state => reducer(state, action))
+  });
 
-    }
-}
-
-export class Provider extends Component {
-    state = {
-        contacts: [ ],
-
-        dispatch: action =>  this.setState(state => reducer(state, action) )
-        
+  useEffect(() => {
+    const fetchContacts = async () => {
+      try {
+        const res = await axios.get('https://jsonplaceholder.typicode.com/users');
+        setState(prevState => ({ ...prevState, contacts: res.data }));
+      } catch (error) {
+        console.log(error);
+      }
     };
 
-   async componentDidMount(){
-        const res = await axios
-        .get('https://jsonplaceholder.typicode.com/users')
-        
-        this.setState({contacts: res.data})
-    }
+    fetchContacts();
+  }, []);
 
-    render() {
-        return (
-            <Context.Provider value = {this.state}>
-                {this.props.children}
-            </Context.Provider>
-
-        )
-    }
-
-    
-}
+  return <Context.Provider value={state}>{children}</Context.Provider>;
+};
 
 export const Consumer = Context.Consumer;
